3c9344a2051538b5c63ad9af6daa99b850636f55
maxdhn <maxduchan@gmail.com>
2018-10-29 17:15:24 +0100

    fact component

--- src/app/fact/fact.component.ts
+++ src/app/fact/fact.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Fact } from '../common/fact';
+import { FactService } from '../common/fact.service';
+import { ActivatedRoute } from '@angular/router';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-fact',
@@ -6,10 +10,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fact.component.css']
 })
 export class FactComponent implements OnInit {
+  fact: Fact = new Fact();
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private service: FactService) { }
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe(res => {
+      const id = this.activatedRoute.snapshot.params.id;
+      this.service.readById(id).subscribe(data => {
+        this.fact = data;
+      });
+    });
   }
 
 }
